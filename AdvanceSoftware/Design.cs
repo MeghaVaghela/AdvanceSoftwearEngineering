dbd3e56a47e0a40970eea9a59d1c6485001a3f43
MeghaVaghela <meghavaghela28@gmail.com>
2023-10-30 23:50:26 +0000

    Variable declaration in design file

--- AdvanceSoftware/Design.cs
+++ AdvanceSoftware/Design.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace AdvanceSoftware
 {
     internal class Design
     {
+        //Graphics variable to execute graphics fuction
+        Graphics graph;
+        //declaire pen variable
+        Pen pen;
+        //declare two integer variable x and y which show currunt position of ponter 
+        int x, y;
+        //declare current position recangle to show current pointer
+        Rectangle CuurPos;
+        //declare current shape recangle to for getting current shape
+        Rectangle CurrShape;
+
+
     }
 }
