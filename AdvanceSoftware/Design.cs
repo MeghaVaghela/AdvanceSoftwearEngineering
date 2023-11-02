74672b1e55ec0ff13f819f5e89f8f580fef18e45
MeghaVaghela <meghavaghela28@gmail.com>
2023-11-02 19:17:34 +0000

    Square Implementation in Design file

--- AdvanceSoftware/Design.cs
+++ AdvanceSoftware/Design.cs
@@ -16,10 +16,49 @@ namespace AdvanceSoftware
         //declare two integer variable x and y which show currunt position of ponter 
         int x, y;
         //declare current position recangle to show current pointer
-        Rectangle CuurPos;
+        Rectangle Cuurent_Position;
         //declare current shape recangle to for getting current shape
-        Rectangle CurrShape;
+        Rectangle Current_Shape;
 
+        public Design()
+        {
+            this.graph = Graphics.FromImage(DesignValues.newPic);
+            pen =   new Pen(Color.Black, 1);
+            x = DesignValues.x;
+            y = DesignValues.y;
+       
+        }
+
+        public void DrawSquare(int width)
+        {
+            try
+            {
+                int x_pos = x - (width / 2);
+                int y_pos = y - (width / 2);
+                Current_Shape = new Rectangle(x_pos, y_pos, width, width);
+                if(DesignValues.isFill) 
+                {
+                    this.graph.FillRectangle(DesignValues.fillColour, Current_Shape);
+                    this.graph.DrawRectangle(pen, Current_Shape);
+                    DesignValues.isUnitTestValid = true;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                PrintMessage(ex.Message);
+                DesignValues.isUnitTestValid=false;
+            }
+
+        }
+
+        public void PrintMessage(string Error_Message) 
+        {
+            using(Font MyFont = new Font("Arial", 9))
+            {
+                graph.DrawString(Error_Message, MyFont, Brushes.Gray,new Point(5,5));
+            }
+        }
 
     }
 }
