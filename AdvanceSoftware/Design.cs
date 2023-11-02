9765c5f651ad3fb4e8668577f14e72a0b2d2d77c
MeghaVaghela <meghavaghela28@gmail.com>
2023-11-02 23:35:10 +0000

    More Implementation done in Design file

--- AdvanceSoftware/Design.cs
+++ AdvanceSoftware/Design.cs
@@ -16,7 +16,7 @@ namespace AdvanceSoftware
         //declare two integer variable x and y which show currunt position of ponter 
         int x, y;
         //declare current position recangle to show current pointer
-        Rectangle Cuurent_Position;
+        Rectangle Current_Position;
         //declare current shape recangle to for getting current shape
         Rectangle Current_Shape;
 
@@ -93,12 +93,55 @@ namespace AdvanceSoftware
                 DesignValues.isUnitTestValid= false;
             }
         }
+
+        public void DrawRectangle(int width, int height)
+        {
+            try
+            {
+                int x_pos = x - (width / 2);
+                int y_pos = y - (width / 2);
+                Current_Shape = new Rectangle(x_pos,y_pos,width,height);
+                if (DesignValues.isFill)
+                    this.graph.FillRectangle(DesignValues.fillColour, Current_Shape);
+                this.graph.DrawRectangle(pen, Current_Shape);
+            }
+            catch (Exception ex) { 
+                PrintMessage(ex.Message);
+                DesignValues.isUnitTestValid = false;
+            }
+        }
+
+        public void MovePoint(int x_pos, int y_pos)
+        {
+            try
+            {
+                pen = new Pen(SystemColors.ActiveBorder, 2);
+                this.graph.DrawRectangle(pen, Current_Position);
+                pen = new Pen(Color.Blue,2);
+                Current_Position = GetRectangle(x_pos, y_pos, 2, 2);
+                this.graph.DrawRectangle(pen, Current_Position);
+                x = DesignValues.x = x_pos;
+                y = DesignValues.y = y_pos;
+                pen = new Pen(Color.Gray, 1);
+                DesignValues.isUnitTestValid = true;
+            }
+            catch(Exception ex)
+            {
+                PrintMessage(ex.Message);
+                DesignValues.isUnitTestValid=false;
+            }
+        }
         public void PrintMessage(string Error_Message) 
         {
             using(Font MyFont = new Font("Arial", 9))
             {
                 graph.DrawString(Error_Message, MyFont, Brushes.Gray,new Point(5,5));
             }
+        }
+
+        private Rectangle GetRectangle(int Rec_x, int Rec_y, int Rec_width, int Rec_height)
+        {
+            return new Rectangle(Rec_x, Rec_y, Rec_width, Rec_height);
         }
 
     }
