05aca8a6022cc3c0944968d8696ffbe5cca93357
MeghaVaghela <meghavaghela28@gmail.com>
2024-01-03 01:10:24 +0000

    Draw Circle file completed

--- ASE_Part_2/drawCircle.cs
+++ ASE_Part_2/drawCircle.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace ASE_Part_2
 {
-    internal class drawCircle
+    internal class drawCircle 
     {
+        int diameter;
+
+        public drawCircle(int diameter)
+        { this.diameter = diameter; }
+
+        public drawCircle(int x , int y,int diameter):base(x,y)
+        {
+            this.diameter = diameter;
+        }
+
+        public drawCircle():base()
+        { }
+
+        public override void Draw(Graphics graph, Pen Pen, Brush brush)
+        {
+            graph.FillEllipse(brush, x, y, diameter * 2, diameter * 2);
+            graph.DrawEllipse(Pen, x, y, diameter * 2, diameter * 2);
+        }
+
+        public override void set(ParameterizedThreadStart int[] list)
+        {
+            base.set(list[0], list[1]);
+            this.diameter = list[2];
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.diameter;
+        }
     }
 }
