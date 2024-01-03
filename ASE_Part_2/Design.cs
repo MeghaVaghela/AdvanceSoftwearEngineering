9077987cc13cd45573540781f1553c3276c4b01f
MeghaVaghela <meghavaghela28@gmail.com>
2024-01-03 01:30:13 +0000

    Draw Rectangle file completed

--- ASE_Part_2/Design.cs
+++ ASE_Part_2/Design.cs
@@ -8,5 +8,6 @@ namespace ASE_Part_2
 {
     internal class Design
     {
+
     }
 }
--- ASE_Part_2/drawRectangle.cs
+++ ASE_Part_2/drawRectangle.cs
@@ -8,5 +8,29 @@ namespace ASE_Part_2
 {
     internal class drawRectangle
     {
+        private int width;
+        private int height;
+
+        public drawRectangle(int x, int y, int width, int height) : base(x, y)
+        { 
+            this.width = width; 
+            this.height = height; 
+        }
+
+        public drawRectangle() { }
+
+        public override void set(Parameter int[] list)
+        {
+            base.set(list[0], list[1]);
+            this.width = width;
+            this.height = height;
+        }
+
+        public override void Draw(Graphics graph, Pen pen, Brush brush)
+        {
+            graph.FillRectangle(brush, this.width, this.height);
+            graph.DrawRectangle(pen, this.width, this.height);
+        }
+
     }
 }
