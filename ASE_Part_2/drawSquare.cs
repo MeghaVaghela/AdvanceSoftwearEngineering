44cb984bbd62f45fd5b6ec01bf277d743e8a018a
MeghaVaghela <meghavaghela28@gmail.com>
2024-01-04 15:59:37 +0000

    Draw Square file implemented

--- ASE_Part_2/drawSquare.cs
+++ ASE_Part_2/drawSquare.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace ASE_Part_2
 {
-    internal class drawSquare
+    public class drawSquare
     {
+        readonly int size;
+
+        public drawSquare(int x, int y,int size): base(x,y,size,size)
+        {
+            this.size = size;
+        }
+
+        public override void Draw(Graphics graph, Pen pen, Brush brush)
+        {
+            base.Draw(graph, pen, brush);
+        }
     }
 }
