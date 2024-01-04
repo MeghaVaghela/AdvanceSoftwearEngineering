1ec6820abaef28761477e3dd9bea23e4615a9172
MeghaVaghela <meghavaghela28@gmail.com>
2024-01-04 19:40:00 +0000

    Design Factory class implemented

--- ASE_Part_2/designFactory.cs
+++ ASE_Part_2/designFactory.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace ASE_Part_2
 {
-    internal class designFactory
+    public class designFactory
     {
+        public Design GetDesign(String DesignType)
+        {
+            DesignType = DesignType.ToLower().Trim();
+
+            if(DesignType.Equals("circle"))
+            {
+                return new drawCircle();
+            }
+            else if(DesignType.Equals("square"))
+            {
+                return new drawSquare();
+            }
+            else if(DesignType.Equals("triangle"))
+            {
+                return new drawTriangle();
+            }
+            else if (DesignType.Equals("rect"))
+            {
+                return new drawRectangle();
+            }
+            else
+            {
+                System.ArgumentException argumentException = new System.ArgumentException("Factory Exception occur :" + DesignType + " is not available");
+            }
+        }
     }
 }
+;
