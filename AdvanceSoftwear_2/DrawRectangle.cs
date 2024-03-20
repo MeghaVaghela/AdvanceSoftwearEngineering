4338336d832a1cd9148f5b6274d99ac8f3217780
MeghaVaghela <meghavaghela28@gmail.com>
2024-03-20 20:54:06 +0000

    Rectangle class implementation done

--- AdvanceSoftwear_2/DrawRectangle.cs
+++ AdvanceSoftwear_2/DrawRectangle.cs
@@ -8,5 +8,31 @@ namespace AdvanceSoftwear_2
 {
     internal class DrawRectangle
     {
+        private int width;
+        private int height;
+
+        public DrawRectangle(int x, int y, int width, int height) : base(x, y)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        
+        public DrawRectangle() { }
+
+        
+        public override void set(params int[] list)
+        {
+            base.set(list[0], list[1]);
+            this.width = list[2];
+            this.height = list[3];
+        }
+
+        
+        public override void Draw(Graphics graph, Pen pen, Brush brush)
+        {
+            graph.FillRectangle(brush, x, y, width, height);
+            graph.DrawRectangle(pen, x, y, width, height);
+        }
     }
 }
