3ac60da2aea294e5ceee77c55f1c14566c3c6dc6
MeghaVaghela <meghavaghela28@gmail.com>
2024-03-20 21:52:28 +0000

    Square and Triangle  class implementation done

--- AdvanceSoftwear_2/DrawSquare.cs
+++ AdvanceSoftwear_2/DrawSquare.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,21 @@ namespace AdvanceSoftwear_2
 {
     internal class DrawSquare
     {
+        readonly int size;
+
+        public DrawSquare()
+        {
+        }
+
+        public DrawSquare(int x, int y, int size) : base(x, y, size, size)
+        {
+            this.size = size;
+        }
+
+        public override void Draw(Graphics graph, Pen pen, Brush brush)
+        {
+            base.Draw(graph, pen, brush);
+        }
+
     }
 }
--- AdvanceSoftwear_2/DrawTriangle.cs
+++ AdvanceSoftwear_2/DrawTriangle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,25 @@ namespace AdvanceSoftwear_2
 {
     internal class DrawTriangle
     {
+        private Point[] point;
+
+        public DrawTriangle(Point[] point)
+        {
+            this.point = point;
+        }
+        public DrawTriangle() { }
+
+        public override void setTriangle(int x, int y, Point[] point)
+        {
+            base.set(x, y);
+            this.point = point;
+        }
+
+        public override void Draw(Graphics graph, Pen pen, Brush brush)
+        {
+            graph.DrawPolygon(pen, point);
+            graph.FillPolygon(brush, point);
+        }
+
     }
 }
